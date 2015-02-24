748aebe654d8254555e11a62f181f0f52dec3262
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-24 17:09:54 +0900

    Implement Net::FTP::PortCommand#makeport and prepend it.

--- lib/net/ftp/port_command.rb
+++ lib/net/ftp/port_command.rb
@@ -7,6 +7,21 @@ module Net
         @data_listening_host = host
         @data_listening_port = port
       end
+
+      def makeport
+        if @data_listening_host || @data_listening_port
+          sock = TCPServer.open(@sock.addr[3], @data_listening_port || 0)
+          port = @data_listening_port || sock.add[1]
+          host = @data_listening_host || sock.addr[3]
+          sendport(host, port)
+          return sock
+        else
+          super
+        end
+      end
+      private :makeport
     end
+
+    prepend PortCommand
   end
 end
