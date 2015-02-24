88dbf827e99462cc9b6af3e97275a70e6a32c748
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-24 16:26:53 +0900

    Initial commit.

---
+++ lib/net/ftp/port_command.rb
@@ -0,0 +1,9 @@
+require "net/ftp/port_command/version"
+
+module Net
+  module Ftp
+    module PortCommand
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/net/ftp/port_command/version.rb
@@ -0,0 +1,7 @@
+module Net
+  module Ftp
+    module PortCommand
+      VERSION = "0.0.1"
+    end
+  end
+end
