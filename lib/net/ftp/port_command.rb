a73d883c4326ef215d3c290c1cb0b842a209132e
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-24 17:09:03 +0900

    Net::FTP::PortCommand#port

--- lib/net/ftp/port_command.rb
+++ lib/net/ftp/port_command.rb
@@ -3,6 +3,10 @@ require "net/ftp/port_command/version"
 module Net
   class FTP
     module PortCommand
+      def port(host, port)
+        @data_listening_host = host
+        @data_listening_port = port
+      end
     end
   end
 end
