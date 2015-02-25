23e5ac3c6b5c23bc3f6b8ad6a81b1174d50f11f9
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2015-02-25 09:06:14 +0900

    Setup RSpec.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require File.expand_path("../../lib/net/ftp/port_command", __FILE__)
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.warnings = true
+  config.order = :random
+end
